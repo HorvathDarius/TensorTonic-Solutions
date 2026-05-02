88d4946665f18c4ff3aa9e2e35d20cb247463694
Horvath Darius <61802273+HorvathDarius@users.noreply.github.com>
2026-05-02 20:47:06 +0200

    Add solution for rmsprop-optimizer

---
+++ rmsprop-optimizer/rmsprop-optimizer.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def rmsprop_step(w, g, s, lr=0.001, beta=0.9, eps=1e-8):
+    """
+    Perform one RMSProp update step.
+    """
+    # Write code here
+
+    # w = parameters
+    # g = gradients
+    # s = squared gradient accumulator
+    # lr = learning rate
+    # beta = decay factor
+    # eps = stability constant
+
+    # Step 0. Convert to numpy arrays
+    w_t1 = np.array(w, dtype=float)
+    g_t = np.array(g, dtype=float)
+    s_t1 = np.array(s, dtype=float)
+
+    # Step 1. Update Running average
+    s_t = (beta * s_t1) + ((1 - beta) * g_t * g_t)
+
+    # Step 2. Parameter update
+    w_t = w_t1 - (lr / np.sqrt(s_t + eps) * g_t)
+
+    return (w_t, s_t)
